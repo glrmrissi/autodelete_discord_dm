cb9af4d7b50a84432689a507a3ea16d065ab72a2
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2025-08-09 20:15:14 -0300

    fix: bug when threshold gap

--- main.py
+++ main.py
@@ -14,31 +14,32 @@ def play_deleted():
             nencontrado = pyautogui.locateCenterOnScreen("imgs/user.png", confidence=0.9)
             if nencontrado:
                 x, y = nencontrado
-                deslocamento = 299  
-                pyautogui.mouseDown(x + deslocamento, y)  
-                pyautogui.mouseUp(x + deslocamento, y)  
+                deslocamento = 300 
+                pyautogui.mouseDown(x + deslocamento, y + -10)  
+                pyautogui.mouseUp(x + deslocamento, y + -10)  
                 pyautogui.keyDown("shift")
-                time.sleep(0.2) 
+                time.sleep(0.1) 
                 print(f"Ícone 'nencontrado' encontrado em: {nencontrado}")
                 icone = pyautogui.locateCenterOnScreen("imgs/icone.png")
                 if icone:
                     print(f"Ícone principal encontrado em: {icone}")
                     x2, y2 = icone 
                     pyautogui.mouseDown(x2, y2)  
-                    time.sleep(0.2)  
+                    time.sleep(0.1)  
                     pyautogui.mouseUp(x2, y2)
                     pyautogui.keyUp("shift")
                 else:
-                    time.sleep(0.5)
+                    pyautogui.scroll(scroll_amount)
+                    time.sleep(0.1)
             else:
                 pyautogui.keyUp("shift")
                 print("Ícone 'bab.png' não encontrado, rolando a tela pra cima...")
-                time.sleep(0.2)
+                time.sleep(0.1)
         except Exception as e:
             pyautogui.scroll(scroll_amount)
             pyautogui.keyUp("shift")
             print("Erro:", e)
-            time.sleep(0.2)
+            time.sleep(0.1)
 
 def start_play():
     stop_event.clear()
@@ -66,6 +67,4 @@ btn_stop.pack(padx=10, pady=10)
 hotkey_thread = Thread(target=listen_hotkey, daemon=True)
 hotkey_thread.start()
 
-
 root.mainloop()
-
